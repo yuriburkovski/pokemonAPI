44b558cca93244cf68289fcd12d06172fc57a9a3
yuriburkovski <yuriiburkovskyi@gmail.com>
2020-10-03 11:49:26 +0200

    initial commit

---
+++ src/main/java/pl/sdaacademy/PokemonAcademyApi/PokemonAcademyApiApplication.java
@@ -0,0 +1,13 @@
+package pl.sdaacademy.PokemonAcademyApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonAcademyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonAcademyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sdaacademy/PokemonAcademyApi/PokemonAcademyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sdaacademy.PokemonAcademyApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonAcademyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
