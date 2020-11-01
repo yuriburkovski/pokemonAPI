04fa08f3a023b1690a00e43899f7ac9583a73b84
yuriburkovski <yuriiburkovskyi@gmail.com>
2020-11-01 20:33:34 +0100

    fix(paa): add exception handler for user registration service

--- src/main/java/pl/sdaacademy/PokemonAcademyApi/common/web/ErrorHandler.java
+++ src/main/java/pl/sdaacademy/PokemonAcademyApi/common/web/ErrorHandler.java
@@ -5,12 +5,24 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import pl.sdaacademy.PokemonAcademyApi.common.service.NoPokemonFoundException;
+import pl.sdaacademy.PokemonAcademyApi.registration.service.UserAlreadyExistException;
+import pl.sdaacademy.PokemonAcademyApi.registration.service.UserNotFoundException;
 
 @ControllerAdvice
 public class ErrorHandler {
 
     @ExceptionHandler(value = NoPokemonFoundException.class)
-    public ResponseEntity<Object> exception(NoPokemonFoundException exception) {
+    public ResponseEntity<Object> notFoundException(NoPokemonFoundException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(value = UserAlreadyExistException.class)
+    public ResponseEntity<Object> userAlreadyExistException(UserAlreadyExistException exception){
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(value = UserNotFoundException.class)
+    public ResponseEntity<Object> userNotFoundException(UserNotFoundException exception){
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
     }
 
