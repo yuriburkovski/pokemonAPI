45cc056bae0bce795e07c3a1a8e5cb37458ddb68
yuriburkovski <yuriiburkovskyi@gmail.com>
2020-11-15 20:26:25 +0100

    [MOD] add admin user to DB by default feature

--- src/main/java/pl/sdaacademy/PokemonAcademyApi/config/CommandLineAppStartRunner.java
+++ src/main/java/pl/sdaacademy/PokemonAcademyApi/config/CommandLineAppStartRunner.java
@@ -4,20 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import pl.sdaacademy.PokemonAcademyApi.registration.repository.PokemonApiUser;
-import pl.sdaacademy.PokemonAcademyApi.registration.repository.PokemonApiUserRepository;
+import pl.sdaacademy.PokemonAcademyApi.registration.service.PokemonUserApiService;
 
 @Component
 public class CommandLineAppStartRunner implements CommandLineRunner {
-    private PokemonApiUserRepository pokemonApiUserRepository;
+    private final PokemonUserApiService pokemonUserApiService;
 
     @Autowired
-    public CommandLineAppStartRunner(PokemonApiUserRepository pokemonApiUserRepository) {
-        this.pokemonApiUserRepository = pokemonApiUserRepository;
+    public CommandLineAppStartRunner(PokemonUserApiService pokemonUserApiService) {
+        this.pokemonUserApiService = pokemonUserApiService;
     }
 
     @Override
     public void run(String... args) throws Exception {
         PokemonApiUser user = new PokemonApiUser("admin", "test");
-        pokemonApiUserRepository.save(user);
+        pokemonUserApiService.addUser(user);
     }
 }
